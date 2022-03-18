7ec39f8b0e81f040248066032faea9ecd62228a3
Martin Richtarsky <s@martinien.de>
2022-03-18 17:24:49 +0100

    Fix backtick

--- keyboards/ergodox_ez/keymaps/martin_colemak/keymap.c
+++ keyboards/ergodox_ez/keymaps/martin_colemak/keymap.c
@@ -284,7 +284,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     break;
     case ST_MACRO_3:
     if (record->event.pressed) {
-      SEND_STRING(SS_LSFT(SS_TAP(X_GRAVE)) SS_DELAY(100) SS_TAP(X_SPACE));
+      SEND_STRING(SS_TAP(X_GRAVE) SS_DELAY(100) SS_TAP(X_SPACE));
 
     }
     break;
