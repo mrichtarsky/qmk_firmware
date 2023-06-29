c802fb6089cad1c4d11cb93d33daf88727ce1cd9
Martin Richtarsky <s@martinien.de>
2023-06-29 10:47:16 +0200

    fix alt, reduce TAPPING_TERM

--- keyboards/ergodox_ez/keymaps/martin_colemak/config.h
+++ keyboards/ergodox_ez/keymaps/martin_colemak/config.h
@@ -2,10 +2,11 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
-#define ORYX_CONFIGURATOR
+#undef ORYX_CONFIGURATOR
 #define IGNORE_MOD_TAP_INTERRUPT
 #undef TAPPING_TERM
-#define TAPPING_TERM 163
+#define TAPPING_TERM 150
+#define ONESHOT_TAP_TOGGLE 3
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #undef MOUSEKEY_MAX_SPEED
